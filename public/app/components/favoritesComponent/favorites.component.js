c456e8098bfb5f09b95a1648e937ded3f9061f5c
Andrew Cheap <ancheap@gmail.com>
2016-12-11 21:29:58 -0500

    updated favorites

--- public/app/components/favoritesComponent/favorites.component.js
+++ public/app/components/favoritesComponent/favorites.component.js
@@ -8,31 +8,13 @@
 			templateUrl: 'app/components/favoritesComponent/favorites.template.html',
 	});
 
-	function favoritesController(favoritesService, $filter) {
+	function favoritesController(favoritesService) {
 	
 		var self = this;
 		
 		self.userFavorites = favoritesService.getFavorites();
 
-		  	// Use the Angular $filter to set orderBy to the orderBy filter
-		  	var orderBy = $filter('orderBy');
 
-		  	// sortBy function is called when the table head is clicked
-			function sortBy(propertyName) {
-
-			  // Set reverse boolean
-			  // If  propertyName is passed and equals the property name last passed, then toggle the reverse value
-			  self.reverse = (propertyName !== null && self.propertyName === propertyName) ? !self.reverse : false;
-			  
-			  self.propertyName = propertyName;
-
-			  // One all the values are set, then call the orderBy filter.
-			  self.flights = orderBy(self.flights, self.propertyName, self.reverse);
-
-			  // Reset the pagingation to the first page
-			  setPagingData(1);
-
-			};
 
 	}
 })();
