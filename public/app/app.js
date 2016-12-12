1109d361d9538384ba307e20b7af5aa95644e62a
Andrew Cheap <ancheap@gmail.com>
2016-12-11 20:59:29 -0500

    updated appjs for routes and favorites

--- public/app/app.js
+++ public/app/app.js
@@ -2,9 +2,11 @@
 	'use strict';
 	angular.module('trippr.app', [	
 			// external dependencies
-
+		
 			// internal dependencies
-			'trippr.search'
+			'trippr.search',
+			'trippr.favorites',
+			'trippr.routes'
 	]);
 	angular.bootstrap(document, ['trippr.app']);
 })();
